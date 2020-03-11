c77fe191fc51696cc612f9bddd4830270a89d7b5
bmark88 <bradley.mark@hotmail.com>
2020-03-11 16:51:24 +0000

    added move commands

--- client.js
+++ client.js
@@ -12,14 +12,16 @@ const connect = function() {
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
   
-  // notifies us when our connection is established
+  // notifies us when our connection is established, and writes to server
   conn.on('connect', () => {
     console.log('Successfully established connection');
-  })
+    conn.write('Name: BM  ') // writing to server our username (3 characters or less)
+    // conn.write('Move: up'); // move up command
+    // conn.write('Move: down'); // move down command
+    // conn.write('Move: left'); // move left command
+    // conn.write('Move: right'); // move right command
 
-  // writing to server our username (3 characters or less)
-  conn.on('connect', () => {
-      conn.write('Name: BM  ')
+    // setInterval(() => {conn.write('Move: up'), 50}) // automatically moves up every 50ms
   })
 
   // event listener to read what the server is sending us
