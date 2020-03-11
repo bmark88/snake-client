f2534acb5c30383e2f38ee51ad989bf74dbd9756
bmark88 <bradley.mark@hotmail.com>
2020-03-11 17:00:21 +0000

    added setup input, and allow for user to exit the game using ctrl+c

--- play.js
+++ play.js
@@ -1,3 +1,21 @@
 const { connect } = require('./client');
+const stdin = process.stdin;
 console.log('Connecting ...');
+
+const setupInput = () => {
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+};
+
+const handleUserInput = () => {
+  stdin.on('data', (key) => {
+    if (key === '\u0003') { // allows ctrl + C to exit the game
+      process.exit();
+    }
+  })
+};
+
 connect();
+setupInput();
+handleUserInput();
